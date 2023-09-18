e58a3d2c27fbff508f9d1c659e68d265f58f06a2
KyungTae Park <96967183+badmaniacs@users.noreply.github.com>
2023-09-18 17:38:38 +0900

    ♻️ Refactor :  Create table component

---
+++ src/components/unsortedtable.tsx
@@ -0,0 +1,42 @@
+import { Music } from '@/gql/graphql';
+import Songcell from './songcell';
+
+interface UnsortedtableProps {
+  songs: {
+    title: string;
+    difficulty: string;
+    normal: string;
+    hard: string;
+    version: string;
+  }[];
+
+  playdata: Music[];
+}
+
+const Unsortedtable: React.FC<UnsortedtableProps> = ({ songs, playdata }) => {
+  return (
+    <div className="grid grid-cols-7 text-sm items-center border-gray-300 border-t border-l mb-4">
+      <div className="text-sm h-full w-full border-b flex justify-center items-center">
+        <p>미분류</p>
+      </div>
+      <div className="col-span-6 border-l">
+        <div className="grid grid-cols-6 ">
+          {songs.map((song) => (
+            <Songcell
+              key={song.title + song.difficulty}
+              title={song.title}
+              difficulty={song.difficulty}
+              playdata={playdata?.find(
+                (item) =>
+                  item.music_name === song.title &&
+                  item.difficulty === song.difficulty,
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Unsortedtable;
